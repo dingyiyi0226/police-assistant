b265843fd4981071d05a9122f4232b2bee0100cc
Huan-Ting,Chen <dingyiyi0226@gmail.com>
2021-01-14 10:36:44 +0800

    add sidebar on map

--- client/src/crimeMap.js
+++ client/src/crimeMap.js
@@ -3,6 +3,7 @@ import GoogleMap from 'google-map-react'
 
 import './style.css'
 
+const OFFENSE_TYPE = ['All', 'Homicide', 'Forcible rape', 'Robbery', 'Assault', 'Burglary', 'Arson', 'Other']
 const NTULibrary = {lat: 25.0174, lng: 121.5405}
 
 class CrimeMap extends Component {
@@ -37,8 +38,7 @@ class CrimeMap extends Component {
     const infowindows = [];
     console.log('render ', records)
 
-    records.filter(record => true)
-           .forEach(record => {
+    records.forEach(record => {
       markers.push(new maps.Marker({
         position: {
           lat: NTULibrary.lat,
@@ -58,21 +58,58 @@ class CrimeMap extends Component {
     })
   }
 
+  onSelectOption = (e) => {
+    console.log(e.target.value)
+    this.setState({offenseType: e.target.value})
+  }
+
+  filteredRecord = () => {
+    const { offenseType, records } = this.state
+
+    if (offenseType === '' || offenseType.toLowerCase() === 'all'){
+      return records
+    }
+    else {
+      return records.filter( record => record.offenseCode.toLowerCase() === offenseType.toLowerCase() )
+    }
+  }
+
   render () {
     return (
-      <div className="py-3 crime-map__container">
-        <h2>CrimeMap</h2>
-        <div className="crime-map" key={this.state.records.length}>
-          <GoogleMap
-            bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_KEY }}
-            defaultCenter={this.state.mapCenter}
-            defaultZoom={12}
-            yesIWantToUseGoogleMapApiInternals
-            onGoogleApiLoaded={({map, maps}) => this.renderGoogleApi(map, maps, this.state.records)}
-          >
-          </GoogleMap>
+
+      <div className="container">
+        <div className="row">
+
+          <div className="col-3 filter-option">
+          { OFFENSE_TYPE.map( type => (
+            <div className="form-check" key={type}>
+              <input className="form-check-input" type="radio" name="offenseType" value={type} id={type}
+                     onChange={this.onSelectOption} checked={this.state.offenseType===type}/>
+              <label className="form-check-label" htmlFor={type}>
+                {type}
+              </label>
+            </div>
+          ))}
+          </div>
+
+          <div className="col-8">
+            <div className="crime-map__container">
+              <div className="crime-map" key={this.state.records.length+this.state.offenseType}>
+                <GoogleMap
+                  bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_KEY }}
+                  defaultCenter={this.state.mapCenter}
+                  defaultZoom={12}
+                  yesIWantToUseGoogleMapApiInternals
+                  onGoogleApiLoaded={({map, maps}) => this.renderGoogleApi(map, maps, this.filteredRecord())}
+                >
+                </GoogleMap>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
+
     )
   }
     
